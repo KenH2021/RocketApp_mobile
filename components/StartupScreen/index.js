55a9624c52f7232d6855e8ead39c15f84b445b20
Kenh2021 <h_ken01@hotmail.com>
2022-01-13 12:00:51 -0500

    bgk ok

---
+++ components/StartupScreen/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, ImageBackground} from 'react-native';
+import styles from './styles';
+
+const StartupScreen = (props) => {
+  return (
+    <View style={styles.bgkContainer}>
+    <ImageBackground source={require('../../assets/bgk.jpg')} style={styles.images}/>
+
+    <View style={styles.titles}>
+
+      <Text style={styles.title}>Model S</Text>
+      <Text style={styles.subtitle}>Starting at 9099 S</Text>
+      
+    </View>
+  </View>
+  );
+}
+
+export default StartupScreen;
---
+++ components/StartupScreen/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create ({
+    bgkContainer: {
+        width: '100%',
+        height: '100%',
+        },
+    images: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+        },
+    titles: {
+        marginTop: '30%',
+        width: '100%',
+        alignItems:'center',
+        },
+    title: {
+        fontSize: 40,
+        fontWeight: '600',
+        },
+    subtitle: {
+    },
+});
+
+
+export default styles;
