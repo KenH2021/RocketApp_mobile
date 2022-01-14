5e04e5436b12e384be376a884b22495afaf92de1
Kenh2021 <h_ken01@hotmail.com>
2022-01-13 20:44:34 -0500

    navigation ok

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import StartupScreen from './components/StartupScreen'
+import { SafeAreaView, StyleSheet, Text, View, ImageBackground } from 'react-native';
+// import StartupScreen from './components/StartupScreen'
+import Navigation from './components/Navigation';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      <StartupScreen />
-
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Navigation/>
+      {/* <StartupScreen /> */}
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
