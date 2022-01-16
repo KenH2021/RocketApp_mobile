9c921845320d6cfd5bef8450a612c101659a72f9
Kenh2021 <h_ken01@hotmail.com>
2022-01-16 12:15:10 -0500

    Put Call ok

--- components/ElevatorStatus/index.js
+++ components/ElevatorStatus/index.js
@@ -1,11 +1,25 @@
 import React, { Component , useState } from 'react';
 import { View, Text, ImageBackground, Image, Pressable, FlatList, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
+import LogOut from '../LogOut';
+import axios from 'axios';
 
 export default class ElevatorStatus extends React.Component {
-
+  state = {updatedStatus: []}
+  
+  updateElevatorStatus() {
+    console.log(this.props.route.params.id);
+    
+    return axios.put(`https://rocket-ele.herokuapp.com/api/Elevators/changeStatus/${this.props.route.params.id}`)
+    .then(res => {
+      const updatedStatus = res.data;
+      this.setState({updatedStatus});
+    })
+  }
   render() { 
-    const {id, status} = this.props.route.params          
+    const {id, status} = this.props.route.params 
+    
   return (
+
     <View style={styles.listContainer}>
       <Text style={styles.header}>
         Elevator List
@@ -14,7 +28,11 @@ export default class ElevatorStatus extends React.Component {
         Elevator #:{id}
         Status:{status}
       </Text>
+      <Text>
+      <LogOut text="Update Elevator Status" onPress={() => this.updateElevatorStatus()} />
+      </Text>
     </View>
+
     );
    }
   }
@@ -28,6 +46,7 @@ const styles = StyleSheet.create ({
     padding: 3,      
   },
   listContainer: {
+    width: "100%",
     alignItems:'center',
     backgroundColor: '#ddd',
     marginVertical: 5,
--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -4,9 +4,9 @@ import { View, Text, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import StartupScreen from '../StartupScreen';
 import HomeScreen from '../HomeScreen';
 import ElevatorStatus from '../ElevatorStatus';
+import StartupScreen from '../StartupScreen';
 
 
 
@@ -16,9 +16,9 @@ const Navigation = () => {
   return (
     <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="SignIn" component={StartupScreen}/>
           <Stack.Screen name="Home" component={HomeScreen}/>
           <Stack.Screen name="Elevator" component={ElevatorStatus}/>
+          <Stack.Screen name="SignIn" component={StartupScreen}/>
         </Stack.Navigator>
         
     </NavigationContainer>
